0a7ac387e5e411b619217595dbd8ce2b90b4b435
arjoonn <106368530+arjoonn@users.noreply.github.com>
2022-08-03 23:48:38 +0530

    Create selection.c

---
+++ selection.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int i,j,limit,a[25],temp,min;
+ printf("enter the limit of the array\n");
+ scanf("%d",&limit);
+ printf("enter %d elements\n",limit);
+ for(i=1;i<=limit;i++)
+  scanf("%d",&a[i]);
+ printf("\n");
+ for(i=0;i<=limit;i++)
+{
+        min=i;
+ for(j=i+1;j<=limit;j++)
+{
+ if(a[min]>a[j])
+
+  min=j;
+ }
+ if(min!=i)
+ {
+ temp=a[i];
+ a[i]=a[min];
+ a[min]=temp;
+ }
+}
+ printf("the array after sorting is\n") ;
+ for(i=1;i<=limit;i++)
+ {
+  printf("%d\n",a[i]);
+ }
+ getch();
+}
